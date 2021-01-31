20ed939a0d9dcc355fcb084433a2cbb050a6925c
avrm <avrmeduard@gmail.com>
2021-01-31 18:42:38 +0200

    jwt request filter created

---
+++ src/main/java/com/avrm/springsecurityjwt/filter/JwtsRequestFilter.java
@@ -0,0 +1,20 @@
+package com.avrm.springsecurityjwt.filter;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+public class JwtsRequestFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+
+    }
+}
